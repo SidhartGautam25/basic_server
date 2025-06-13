5f8633c71b49c93e3134e764266a5f29abc9b3a6
sidhart25 <navneetnishchal1420@gmail.com>
2025-06-13 11:48:33 +0530

    router is now inside the library

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const BasicServer = require("./lib/server.js");
+import { BasicServer } from "./lib/server.js";
 
-function createServer(opts = {}) {
+export function createServer(opts = {}) {
   const basic_server = new BasicServer(opts);
 
   // for extra config things
   return basic_server;
 }
-
-module.exports = createServer;
